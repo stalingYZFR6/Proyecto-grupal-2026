2d7a18e744dfa2fe0c0d9f9413409764ef841bb2
[dyad] <sequeiramedinag@gmail.com>
2026-06-11 09:59:14 -0600

    Remove 2 files

--- src/components/test/suma.test.js
+++
@@ -1,7 +0,0 @@
-// prueba real 
-
-const sum = require('./suma') // importar funcion 
-
-test("la funcion suma debe devolver suma correcta", () => { // definir el test
-    expect(sum(1, 2)).toBe(3);
-});
